f6735731836e73340a7cfa19d1278593a363c4d0
Maxime <maxime.sahuc@univ-rouen.fr>
2024-09-11 10:57:27 +0200

    pow func: add edge cases

--- fonctions.py
+++ fonctions.py
@@ -8,7 +8,14 @@ def puissance(a, b):
     except ValueError as e:
         raise TypeError("Que des integer !!!")
 
+    if b < 0 and a == 0:
+        raise ZeroDivisionError()
+
+    if b == 0:
+        return 1
+
     value = a if b >= 0 else 1/a
+
     for i in range(1, abs(b)):
         if b >= 0:
             value *= a
--- test.py
+++ test.py
@@ -5,11 +5,14 @@ import fonctions as f
 def test_1():
     assert f.puissance(2, 3) == 8
     assert f.puissance(2, 2) == 4
+    assert f.puissance(0, 23) == 0
+    assert f.puissance(353, 0) == 1
 
 
 def test_2():
     assert f.puissance(-1, -5) == -1
     assert f.puissance(23, -3) == 8.218952905399852e-05
+    assert f.puissance(-353, 0) == 1
 
 
 def test_3():
@@ -17,3 +20,5 @@ def test_3():
         f.puissance("a", 1)
     with pytest.raises(TypeError):
         f.puissance("a", "b")
+    with pytest.raises(ZeroDivisionError):
+        f.puissance(0, -4)
