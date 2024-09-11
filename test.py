014bb8fbfd068bba46d83ff6f937a9641bf76489
Maxime <maxime.sahuc@univ-rouen.fr>
2024-09-11 10:19:55 +0200

    pytest: add raises tests

--- test.py
+++ test.py
@@ -11,3 +11,9 @@ def test_2():
     assert f.puissance(-1, -5) == -1
     assert f.puissance(23, -3) == 8.218952905399852e-05
 
+
+def test_3():
+    with pytest.raises(TypeError):
+        f.puissance("a", 1)
+    with pytest.raises(TypeError):
+        f.puissance("a", "b")
