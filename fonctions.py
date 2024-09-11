d30577f97c7bb0f5907d03edb19cb89b399534f5
Maxime <maxime.sahuc@univ-rouen.fr>
2024-09-11 10:41:25 +0200

    regression

--- fonctions.py
+++ fonctions.py
@@ -8,5 +8,12 @@ def puissance(a, b):
     except ValueError as e:
         raise TypeError("Que des integer !!!")
 
-    return a ** b
+    value = a if b >= 0 else 1/a
+    for i in range(1, abs(b)):
+        if b >= 0:
+            value *= a
+        else:
+            value /= a
+
+    return value
 
